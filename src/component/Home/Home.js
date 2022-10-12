d8e5cba01810a7ca11df4acf0916ee9d93500cea
Mehedi2000 <mh4054528@gmail.com>
2022-10-12 15:13:51 +0600

    added responsiveness for header

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -8,9 +8,13 @@ const Home = () => {
     let quizesTopicData = quizesTopic.data;
     return (
         <div className='mt-8 bg-violet-300 py-10'>
-            <div>
-                <p className='text-2xl font-bold mb-2'>Welcome to Quiz Coconut</p>
-                <p className='font-medium'>Quiz coconut is a quiz website for beginner.It provides some question for importent topics like React,Javascript,HTML,Github etc.</p>
+            <div className='w-3/4 mx-auto grid grid-cols-1 sm:grid grid-cols-1 md:flex justify-start lg:flex justify-start'>
+                <div className='1/4'>
+                    <img className='w-3/4 rounded-xl' src="https://blog.hubspot.com/hubfs/google-quiz.jpg" alt="" />
+                </div>
+                <span className='w-3/4 mt-2'>
+                    <p className='font-medium text-xl'>Quiz coconut is a quiz website for beginner.It provides some question for importent topics like React,Javascript,HTML,Github etc.</p>
+                </span>
             </div>
             <div className='grid grid-cols-1 w-3/4 mx-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10'>
                 {
