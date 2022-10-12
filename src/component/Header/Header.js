a134fd7e3c71ffc48f5d55c49e3e9ab9b7e8d92d
Mehedi2000 <mh4054528@gmail.com>
2022-10-12 19:03:43 +0600

    added redmi

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
 
@@ -6,14 +6,14 @@ const Header = () => {
     return (
         <div>
             <div className="navbar bg-violet-500">
-                <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-2xl font-bold text-white">Quiz Coconut</a>
+                <div className="flex-1 ml-8">
+                    <a href="" alt="" className="btn btn-ghost normal-case text-2xl font-bold text-white">Quiz Coconut</a>
                 </div>
                 <div className="flex-none">
-                    <ul className="menu menu-horizontal p-0">
-                        <Link to={'/'}><li className='font-medium text-white'><a>Home</a></li></Link>
-                        <Link to={'/statistics'}><li className='font-medium text-white'><a>Statistics</a></li></Link>
-                        <Link to={'/blog'}><li className='font-medium text-white'><a>Blog</a></li></Link>
+                    <ul className={`menu menu-horizontal p-0}`}>
+                        <Link to={'/'}><li className='font-medium mr-2 text-white'><a href="" alt="">Home</a></li></Link>
+                        <Link to={'/statistics'}><li className='font-medium mr-2 text-white'><a href="" alt="">Statistics</a></li></Link>
+                        <Link to={'/blog'}><li className='font-medium mr-2 text-white'><a href="" alt="">Blog</a></li></Link>
                     </ul>
                 </div>
             </div>
